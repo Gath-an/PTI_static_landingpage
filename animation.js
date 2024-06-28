d827ebadebebc93607517b72f634df3874859f9c
Gath-an <150008639+Gath-an@users.noreply.github.com>
2024-06-28 10:37:06 +0700

    Add files via upload

---
+++ animation.js
@@ -0,0 +1,18 @@
+// Let menu = document.querySelector('#menu-icon');
+// Let navlist = document.querySelector('.navlist');
+
+// menu.onclick = () => {
+//     menu.ClassList.toggle('bx-x');
+//     nav-li.ClassList.toggle('open');
+// };
+
+// const sr = ScrollReveal({
+//     distance: '65px',
+//     duration: 2600,
+//     delay: 450,
+//     reset: true
+// })
+
+// sr.reveal('.intro-text',{delay:200, origin:'top'});
+// sr.reveal('.intro-text',{delay:200, origin:'top'});
+// sr.reveal('.intro-text',{delay:200, origin:'top'});
